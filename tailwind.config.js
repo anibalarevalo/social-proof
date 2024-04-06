65bbadd075451f27c4a81327c0b887e0c36c1df6
anibalarevalo <79030561+anibalarevalo@users.noreply.github.com>
2024-04-06 19:18:40 -0400

    modifying the tailwind for more customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'very-dark-magenta': 'hsl(300, 43%, 22%)',
+        'soft-pink': 'hsl(333, 80%, 67%)',
+        'dark-grayish-magenta': 'hsl(303, 10%, 53%)',
+        'light-grayish-magenta': 'hsl(300, 24%, 96%)',
+      },
+
+      fontSize: {
+       base: '15px',
+      },
+
+      fontFamily: {
+        'LS-bold': 'LeagueSpartan-Bold',
+        'LS-medium': 'LeagueSpartan-Medium',
+        'LS-regular': 'LeagueSpartan-Regular',
+      }
+    },
   },
   plugins: [],
 }
